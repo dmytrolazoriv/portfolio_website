e82b4d4826bc893e1ac15aed93e412c25549ec96
dmytrolazoriv <lazarevdmitro1@gmail.com>
2022-07-27 13:14:40 +0300

    Plug and used js cookie

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
     $(this).toggleClass('header_open');
     $('.header__menu-button-wrapper').toggleClass;
     $('.header__menu-button-wrapper_open');
-    $('.header').addClass('header__header_open-mobile');
-    $('.header__social-menu').addClass('header__social-menu_open-mobile');
+    $('.header').toggleClass('header__header-open_mob');
+    $('.header__social-menu').toggleClass('header__social-menu_open-mob');
     $('.header__top').toggleClass('header__top_open');
   });
 });
@@ -120,10 +120,12 @@ $(".dropdownbox").click(function () {
     var selected = $(this).find('.lang-code').html().toUpperCase();
     console.log($(this).find('.lang-code').html().toUpperCase());
     $("#language-selected").html(selected);
+    Cookies.set('langText', selected, { expires: 365 });
     $(".menu-dropdown").removeClass("showMenu");
   });
 });
 
+$("#language-selected").html(Cookies.get('langText'));
 //Close language select box if nothing is selected
 $("#dropdown-wrapper").mouseleave(function () {
   $(".menu-dropdown").removeClass("showMenu");
@@ -225,72 +227,58 @@ $('#contacts__form').on('submit', function (event) {
 //   };
 // });
 
-// Changing value of lang attribute
-var langAttr = function () {
-  $('.lang-en').click(function () {
-    $('html').attr('lang', 'en');
-  });
-  $('.lang-ua').click(function () {
-    $('html').attr('lang', 'ua');
-  });
-}
-
-localStorage.setItem('lang', langAttr);
-langAttr = localStorage.getItem('lang');
-
-$(document).ready(function () {
-  $('.lang-en').click(function () {
-    $('html').attr('lang', 'en');
-  });
-  $('.lang-ua').click(function () {
-    $('html').attr('lang', 'ua');
-  });
-});
-// Changing value of lang attribute
-
-
 // Adding lang attributes for label tags during form validation 
-var labelAttr = function () {
-  $(document).ready(function () {
-    $("#contacts__form").bind("DOMSubtreeModified", function () {
-      $('label').attr('lang', 'ua');  // default value
-
-      $('.lang-en').on("click", function () {
-        $('label').attr('lang', 'en');
-      });
-      $('.lang-ua').on("click", function () {
-        $('label').attr('lang', 'ua');
-      });
-    });
-  });
-}
-
-localStorage.setItem('label', labelAttr);
-labelAttr = localStorage.getItem('label');
-
-$(document).ready(function () {
+$(window).on("load", function () {
   $("#contacts__form").bind("DOMSubtreeModified", function () {
     $('label').attr('lang', 'ua');  // default value
 
     $('.lang-en').on("click", function () {
       $('label').attr('lang', 'en');
     });
+
     $('.lang-ua').on("click", function () {
       $('label').attr('lang', 'ua');
     });
   });
 });
+// localStorage.setItem('label', labelAttr);
+// labelAttr = localStorage.getItem('label');
+// ---- localStorage ----
 // Adding lang attributes for label tags during form validation 
 
+// Changing value of lang attribute
+
+
+$(window).on("load", function () {
+  $('.lang-en').click(function () {
+    $('html').attr('lang', 'en');
+    lang = $('html').attr('lang');
+    Cookies.set('lang', lang, { expires: 365 });
+  });
+  $('.lang-ua').click(function () {
+    $('html').attr('lang', 'ua');
+    lang = $('html').attr('lang');
+    Cookies.set('lang', lang, { expires: 365 });
+  });
+
+  $('html').attr('lang', Cookies.get('lang'));
+});
+
+
+// localStorage.setItem('lang', langAttr);
+// langAttr = localStorage.getItem('lang');
+// ---- localStorage ----
+// Changing value of lang attribute
 
 // Create a cookie
 // localStorage.setItem('lang', langAttr);
 // langAttr = localStorage.getItem('lang');
 
-Cookies.set('lang', langAttr, { expires: 365 });
-Cookies.get('lang'); // => 'value'
-Cookies.set('label', labelAttr, { expires: 365 });
-Cookies.get('label'); // => 'value'
+
+// Cookies.set('lang', langAttr, { expires: 365 });
+// Cookies.get('lang'); // => 'value'
+// Cookies.set('label', labelAttr, { expires: 365 });
+// Cookies.get('label'); // => 'value'
 // Create a cookie
 
 // console.log($('.contacts__form-input.error'));
